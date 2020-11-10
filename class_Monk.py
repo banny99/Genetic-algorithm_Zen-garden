23833945c0ba99c7ee46c03e4b2f32d920eecee1
banny99 <benjamin.bartosik@gmail.com>
2020-11-10 19:15:10 +0100

    kozmeticke upravy kodu

--- class_Monk.py
+++ class_Monk.py
@@ -19,21 +19,16 @@ class Monk:
 
     def rake_garden(self):
 
-        # max_num_of_starting_positions = self.myGarden.size_x - 2 + self.myGarden.size_y - 2 + len(self.myGarden.rocks)
         turn_index = 1
-
         while self.num_of_raked_places < self.myGarden.num_of_sand_places:
 
-            # if len(self.myGarden.free_start_positions) > 0 and len(self.starting_positions) <= max_num_of_starting_positions:
             if len(self.myGarden.free_start_positions) > 0:
                 start_poz = self.set_beginning_poz()
+                self.myPoz_x = start_poz[0]
+                self.myPoz_y = start_poz[1]
+                self.set_initial_move_direction()
             else:
                 break
-
-            # priradenie vybranych pozicii mnichovi +nasledne urcenie smeru pohybu
-            self.myPoz_x = start_poz[0]
-            self.myPoz_y = start_poz[1]
-            self.set_initial_move_direction()
 
             # ak je vobec mozne z danej start. pozicie zacat hrabanie
             if self.is_possible_to_rake():
@@ -45,7 +40,6 @@ class Monk:
                 still_inside_the_garden = True
                 while still_inside_the_garden and self.move_direction != 'x':
                     still_inside_the_garden, norpwol = self.move(turn_index, norpwol)
-
 
                 # ak sa zasekol v zahradke a nevie sa pohnut (=neuspesny tah ->koniec)
                 if self.move_direction == 'x':
@@ -107,110 +101,101 @@ class Monk:
 
 
     def move(self, turn_index, norpwol):
-
         # right:
         if self.move_direction == 'r':
-            garden_poz = self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x + 1]
-            # ak sa moze posunut v jeho smere ->pohrabe a posuva sa
-            if garden_poz == 0 or garden_poz == -1:
-                self.rake_curr_place(turn_index)
-                norpwol += 1
-                self.myPoz_x += 1
-                # ak je este vo vnutri zahrady
-                if garden_poz == 0:
-                    self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x] = 'M'
-                    return True, norpwol
-                # ak sa uz posunul von
-                elif garden_poz == -1:
-                    return False, norpwol
-            else:
-                self.choose_new_direction2()
-                return True, norpwol
-
+            next_poz = self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x + 1]
         # left:
         elif self.move_direction == 'l':
-            garden_poz = self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x - 1]
-            if garden_poz == 0 or garden_poz == -1:
-                self.rake_curr_place(turn_index)
-                norpwol += 1
-                self.myPoz_x -= 1
-                if garden_poz == 0:
-                    self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x] = 'M'
-                    return True, norpwol
-                elif garden_poz == -1:
-                    return False, norpwol
-            else:
-                self.choose_new_direction2()
-                return True, norpwol
-
+            next_poz = self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x - 1]
         # down:
         elif self.move_direction == 'd':
-            garden_poz = self.myGarden.garden_grid[self.myPoz_y + 1][self.myPoz_x]
-            if garden_poz == 0 or garden_poz == -1:
-                self.rake_curr_place(turn_index)
-                norpwol += 1
-                self.myPoz_y += 1
-                if garden_poz == 0:
-                    self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x] = 'M'
-                    return True, norpwol
-                elif garden_poz == -1:
-                    return False, norpwol
-            else:
-                self.choose_new_direction2()
-                return True, norpwol
-
+            next_poz = self.myGarden.garden_grid[self.myPoz_y + 1][self.myPoz_x]
         # up:
         elif self.move_direction == 'u':
-            garden_poz = self.myGarden.garden_grid[self.myPoz_y - 1][self.myPoz_x]
-            if garden_poz == 0 or garden_poz == -1:
-                self.rake_curr_place(turn_index)
-                norpwol += 1
-                self.myPoz_y -= 1
-                if garden_poz == 0:
-                    self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x] = 'M'
-                    return True, norpwol
-                elif garden_poz == -1:
-                    return False, norpwol
-            else:
-                self.choose_new_direction2()
-                return True, norpwol
+            next_poz = self.myGarden.garden_grid[self.myPoz_y - 1][self.myPoz_x]
 
 
-    def rake_curr_place(self, turn_index):
-        if self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x] != -1:
-            self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x] = turn_index
+        # return self.rake_curr_place(next_poz, turn_index, norpwol)
+        still_inside_the_garden, norpwol = self.rake_curr_place(next_poz, turn_index, norpwol)
+        return still_inside_the_garden, norpwol
 
 
-    def choose_new_direction(self):
-        # ak sa nemoze nikam pohnut:
-        if self.is_NOT_possible_to_move():
-            self.move_direction = 'x'
 
+    def rake_curr_place(self, next_poz, turn_index, norpwol):
+        curr_poz = self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x]
+
+        # ak sa da v mojom smere pokracovat
+        if next_poz == 0:
+            # a ak nestojim na okraji ->prepis index tahu
+            if curr_poz != -1:
+                self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x] = turn_index
+            # posun v smere:
+            self.shift_me_in_my_direction()
+            # a oznac moju akt. poz na mape:
+            self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x] = 'M'
+            norpwol += 1
+            return True, norpwol
+
+        # ak sa uz posunul von
+        elif next_poz == -1:
+            if curr_poz != -1:
+                self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x] = turn_index
+            self.shift_me_in_my_direction()
+            norpwol += 1
+            return False, norpwol
+
+        # ak sa neda v mojom smere pokracovat (-prekazka):
         else:
-            if self.move_direction == 'r' or self.move_direction == 'l':
-                d = self.myGarden.garden_grid[self.myPoz_y+1][self.myPoz_x]
-                u = self.myGarden.garden_grid[self.myPoz_y-1][self.myPoz_x]
-                # ak su mozne oba smery ->vyber nahodny z nich
-                if (u == 0 or u == -1) and (d == 0 or d == -1):
-                    self.move_direction = random.choice(['u', 'd'])
-                else:
-                    if u == 0 or u == -1:
-                        self.move_direction = 'u'
-                    elif d == 0 or d == -1:
-                        self.move_direction = 'd'
+            self.choose_new_direction2()
+            return True, norpwol
 
-            elif self.move_direction == 'u' or self.move_direction == 'd':
-                r = self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x+1]
-                l = self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x-1]
-                if (r == 0 or r == -1) and (l == 0 or l == -1):
-                    self.move_direction = random.choice(['l', 'r'])
-                else:
-                    if r == 0 or r == -1:
-                        self.move_direction = 'r'
-                    elif l == 0 or l == -1:
-                        self.move_direction = 'l'
 
-            self.chosen_directions.append(self.move_direction)
+    def shift_me_in_my_direction(self):
+        # right:
+        if self.move_direction == 'r':
+            self.myPoz_x = self.myPoz_x + 1
+        # left:
+        elif self.move_direction == 'l':
+            self.myPoz_x = self.myPoz_x - 1
+        # down:
+        elif self.move_direction == 'd':
+            self.myPoz_y = self.myPoz_y + 1
+        # up:
+        elif self.move_direction == 'u':
+            self.myPoz_y = self.myPoz_y - 1
+
+
+
+    # def choose_new_direction(self):
+    #     # ak sa nemoze nikam pohnut:
+    #     if self.is_NOT_possible_to_move():
+    #         self.move_direction = 'x'
+    #
+    #     else:
+    #         if self.move_direction == 'r' or self.move_direction == 'l':
+    #             d = self.myGarden.garden_grid[self.myPoz_y+1][self.myPoz_x]
+    #             u = self.myGarden.garden_grid[self.myPoz_y-1][self.myPoz_x]
+    #             # ak su mozne oba smery ->vyber nahodny z nich
+    #             if (u == 0 or u == -1) and (d == 0 or d == -1):
+    #                 self.move_direction = random.choice(['u', 'd'])
+    #             else:
+    #                 if u == 0 or u == -1:
+    #                     self.move_direction = 'u'
+    #                 elif d == 0 or d == -1:
+    #                     self.move_direction = 'd'
+    #
+    #         elif self.move_direction == 'u' or self.move_direction == 'd':
+    #             r = self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x+1]
+    #             l = self.myGarden.garden_grid[self.myPoz_y][self.myPoz_x-1]
+    #             if (r == 0 or r == -1) and (l == 0 or l == -1):
+    #                 self.move_direction = random.choice(['l', 'r'])
+    #             else:
+    #                 if r == 0 or r == -1:
+    #                     self.move_direction = 'r'
+    #                 elif l == 0 or l == -1:
+    #                     self.move_direction = 'l'
+    #
+    #         self.chosen_directions.append(self.move_direction)
 
     def choose_new_direction2(self):
         # ak sa nemoze nikam pohnut:
@@ -253,14 +238,6 @@ class Monk:
             if not self.is_possible_to_rake():
                 self.move_direction = old_direction
                 self.choose_new_direction2()
-
-
-    # def is_NOT_possible_to_get_out(self):
-    #     # ak je niektory z nich v startovacich poziciach znamena to ze sa tade moze dostat von
-    #     if (self.myPoz_x + 1, self.myPoz_y) in self.myGarden.free_start_positions or (self.myPoz_x - 1, self.myPoz_y) in self.myGarden.free_start_positions or (self.myPoz_x, self.myPoz_y + 1) in self.myGarden.free_start_positions or (self.myPoz_x, self.myPoz_y - 1) in self.myGarden.free_start_positions:
-    #         return False
-    #     else:
-    #         return True
 
 
     def is_NOT_possible_to_move(self):
